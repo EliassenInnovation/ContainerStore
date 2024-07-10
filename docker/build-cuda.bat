fd488ab1247b442f55825d12e1a80f748f0f5b8a
Matthew Whited <mwhited@eliassen.com>
2024-07-10 10:13:46 -0400

    example docker containers

---
+++ docker/build-cuda.bat
@@ -0,0 +1,4 @@
+
+IF "%APP_PROJECT%"=="" SET APP_PROJECT=container-store
+
+docker compose --project-name %APP_PROJECT% --file docker-compose-cuda.yml build
---
+++ docker/down.bat
@@ -0,0 +1,8 @@
+
+IF "%APP_PROJECT%"=="" SET APP_PROJECT=container-store
+
+SET EXTRA_ARGS=
+IF /I "%1" EQU "ALL" (
+    SET EXTRA_ARGS=--volumes 
+)
+docker compose --project-name %APP_PROJECT% down %EXTRA_ARGS%
